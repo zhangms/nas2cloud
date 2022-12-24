2f1d42883516e0ba1cde4a36faef38f102d52462
zhangmaosheng <zhang.ms@foxmail.com>
2022-12-25 01:50:31 +0800

    加入前端

---
+++ fe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from "./Login";
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Login/>
+        )
+    }
+}
+
+export default App;
