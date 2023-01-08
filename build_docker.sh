d46d880c56a015363e0230911be1d5ee7a584493
zhangmaosheng <zhang.ms@foxmail.com>
2023-01-09 00:27:22 +0800

    nas2cloud-server

--- build_docker.sh
+++ build_docker.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 
 echo "build server..."
-cd server
 export CGO_ENABLED=0
 export GOOS=linux
 export GOARCH=amd64
-go build -o ../release/docker/nas2cloud
-cd ..
+go build -o release/docker/nas2cloud
 
-echo "build fe..."
-cd fe
-npm run build
-cd ..
-rm -rf release/docker/console
-mkdir release/docker/console
-cp -r fe/build/*  release/docker/console
-cd release/docker
-tar cvf console.tar ./console
-rm -rf ./console
--- release/local/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-nohup ./nas2cloud -action=start -profile=local -port=7001 > nas2cloud.log &
-
