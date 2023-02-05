fd521bc2c71154f6280a54bf5c7c5b07b3fa0947
zhangmaosheng <zhang.ms@foxmail.com>
2023-02-06 02:20:28 +0800

    build docker

--- build.sh
+++ build.sh
@@ -61,7 +61,24 @@ build_apk() {
 }
 
 build_dcoker() {
-    echo build docker
+    cd release
+    docker kill nas2cloud
+    docker rm nas2cloud
+    docker rmi nas2cloud
+    docker build -t nas2cloud .
+
+    # docker run \
+    #     --name nas2cloud \
+    #     -p 8168:8168 \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/app:/nas2cloud/app \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/bin:/nas2cloud/bin \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/client:/nas2cloud/client \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/console:/nas2cloud/console \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/thumb:/nas2cloud/thumb \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/assets:/nas2cloud/assets \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/users:/nas2cloud/users \
+    #     -d nas2cloud
+
 }
 
 main(){
