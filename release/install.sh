ad71c5a637466f1a999aadaafe05e3a0c9ba795d
zhangmaosheng <zhang.ms@foxmail.com>
2023-02-06 02:32:31 +0800

    build

---
+++ release/install.sh
@@ -0,0 +1,12 @@
+    # docker run \
+    #     --name nas2cloud \
+    #     -p 8168:8168 \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/app:/nas2cloud/app \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/bin:/nas2cloud/bin \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/client:/nas2cloud/client \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/console:/nas2cloud/console \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/thumb:/nas2cloud/thumb \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/assets:/nas2cloud/assets \
+    #     -v /Users/ZMS/Documents/ProjectBase/github/zhangms/nas2cloud/release/users:/nas2cloud/users \
+    #     -d nas2cloud
+
