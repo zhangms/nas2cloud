d9a35fb28cf5efbb98ffd2c46005dc11af448523
zhangmaosheng <zhang.ms@foxmail.com>
2023-03-12 23:44:44 +0800

    remove

--- build.sh
+++ build.sh
@@ -111,19 +111,3 @@ main(){
     esac
 }
 main $2 $3 $4 $5 $6 $7 $8 $9
-
-# echo "build server..."
-# cd server
-# go build -o ../release/local/nas2cloud
-# cd ..
-
-# echo "build fe..."
-# cd fe
-# npm run build
-# cd ..
-# rm -rf release/local/console
-# mkdir release/local/console
-# cp -r fe/build/*  release/local/console
-
-# rm -rf /Users/ZMS/NAS/local
-# cp -r release/local /Users/ZMS/NAS/
